9590018b9894921461c3eb45b7aabcdd825dcaa5
Jason Robinson <mail@jasonrobinson.me>
2020-10-24 01:00:52 +0300

    Fix with_ratelimit

--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import time
 
 
 async def with_ratelimit(client, method, *args):
-    response = await getattr(client, method, *args)
+    func = getattr(client, method)
+    response = await func(*args)
     if getattr(response, "status_code", None) == "M_LIMIT_EXCEEDED":
         time.sleep(3)
         return with_ratelimit(client, method, *args)
