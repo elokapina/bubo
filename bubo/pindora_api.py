673451c6adf9d04d2005f6c4b9a85ef55707daf3
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2022-09-17 09:59:34 +0300

    Update bubo/pindora_api.py
    Co-authored-by: Jason Robinson <mail@jasonrobinson.me>

--- bubo/pindora_api.py
+++ bubo/pindora_api.py
@@ -16,16 +16,14 @@ def create_new_key(pindora, month, day, start_time, end_time, timezone):
     url = "https://admin.pindora.fi/api/integration/pins"
 
     payload = json.dumps({
-    "validity_rules": [
-        {
-        "pindora": {
-            "id": f"{pindora}"
-        },
-        "date_from": f"2022-{month}-{day}T{start_time}:00+{timezone}:00",
-        "date_to": f"2022-{month}-{day}T{end_time}:00+{timezone}:00"
-        }
-    ],
-    "magic_enabled": True
+        "validity_rules": [{
+            "pindora": {
+                "id": f"{pindora}",
+            },
+            "date_from": f"2022-{month}-{day}T{start_time}:00+{timezone}:00",
+            "date_to": f"2022-{month}-{day}T{end_time}:00+{timezone}:00",
+        }],
+        "magic_enabled": True,
     })
     
 
